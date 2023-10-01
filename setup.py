530687794188b923aafd73bf60697b3a6111d513
praveen <pravintikare30@gmail.com>
2023-10-01 11:01:35 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -18,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='Krish',
-    author_email='krish.naik@pw.live',
+    author='Praveen',
+    author_email='pravintikare30@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
